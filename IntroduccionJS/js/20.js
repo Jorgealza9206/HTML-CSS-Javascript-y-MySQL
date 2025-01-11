aae2c1425c2858c0142012251d9e6f4d24e95777
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-01-11 07:46:29 -0500

    Enero 11 de 2025. Windows. Vimo el final de Fecth API, donde se manejan promises y funciones asíncronas para traernos un objeto JSON. Finalizamos video 144. Revisamos el QuerySelector, aquí podemos seleccionar algún elemento del HTML por medio de clases, id como en CSS. Podemos ver dentro de esa clase o id según sea en Firefox Developer Edition al Inspeccionar elemento que tiene demasiadas características que utilizar. https://www.udemy.com/course/desarrollo-web-completo-con-html5-css3-js-php-y-mysql/learn/lecture/24122756#overview

--- IntroduccionJS/js/20.js
+++ IntroduccionJS/js/20.js
@@ -1,9 +1,30 @@
-function obtenerEmpleados(){
+async function obtenerEmpleados(){
     const archivo = 'js/empleados.json';
-    fetch(archivo)
-        .then (resultado => {
-            console.log(resultado);
-        })
+    // fetch(archivo)
+    //     .then (resultado => {
+    //         //console.log(resultado);
+    //         return resultado.json();
+    //     })
+    //     .then( datos => {
+    //         // console.log(datos);
+
+    //         const { empleados } = datos;
+
+    //         // console.log(empleados);
+
+    //         empleados.forEach(empleado => {
+    //             console.log(empleado);
+    //             console.log(empleado.id);
+    //             console.log(empleado.nombre);
+    //             console.log(empleado.puesto);
+
+    //             document.querySelector('.contenido').textContent = empleado.nombre;
+    //         });
+    //     })
+
+    const resultado = await fetch(archivo);
+    const datos = await resultado.json();
+    console.log(datos);
 }
 
 obtenerEmpleados();
---
+++ JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/script.js
@@ -0,0 +1,9 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2') // 0 o 1 Elementos
+heading.textContent = 'Nuevo Heading';
+heading.classList.add('nueva-clase');
+console.log(heading);
+
+// querySelectorAll
+
+// getElementById
