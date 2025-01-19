7fd366c3160f59f738cca851090adc0fc7ff4bba
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-01-19 13:46:02 -0500

    Enero 19 de 2025.
    Windows. Crear elementos HTML nuevos a partir de Javascript con la funcion document.createElement y con {contenedor}.appendChild agregar ese elemento en dicho contenedor, addEventListener carga primero en document que en window, ya que en document solo espera el HTML mientras que el document espera todo el código CSS y Javascript. Acabamos video 147.
    
    Video 148. Eventos como el click, scrolling y la función preventDefault, el uso del argumento en los callbacks como en 'evento'.
    
    Video 149. Usando el evento input. Minuto 4:20

--- JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/script.js
+++ JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/script.js
@@ -5,5 +5,70 @@ heading.classList.add('nueva-clase');
 console.log(heading);
 
 // querySelectorAll
+//Retorna todos los elementos que concuerdan con la descripción
+const enlaces = document.querySelectorAll('.navegacion a');
+ enlaces[0].textContent = 'Nuevo Texto para Enlace';
+ enlaces[0].classList.add('nueva-clase');
+ //enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
+
+// const heading2 = document.getElementById('heading');
+// console.log(heading2);
+
+//Generar un nuevo enlace o crear un nuevo enlace
+const nuevoEnlace =document.createElement('A');
+
+//Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+//Agregar el texto
+nuevoEnlace.textContent = 'Tienda Virtual';
+//Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//Agregarlo al documento
+const navegacion = document.querySelector('.navegacion')
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
+
+
+//Eventos
+
+console.log(1);
+
+window.addEventListener('load', function() {// load espera a que el JS y los archivos que dependen del HTML estén listos
+    console.log(2);
+});
+
+window.onload = function() {
+    console.log(3);
+}
+
+document.addEventListener('DOMContentLoaded', function() {// Solo espera por el HTML, no espera CSS o imagenes
+    console.log(4);
+});
+
+console.log(5);
+
+window.onscroll = function() {
+    console.log('scrolling...');
+}
+
+// Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(evento) {
+    console.log(evento);
+    evento.preventDefault(); // Evita la acción por defecto
+
+    //Validar un formulario
+    console.log('Enviando Formulario');
+});
+
+// Evento de los inputs y textarea
+
+const nombre = document.querySelector('#nombre');
+
+nombre.addEventListener('input', function(e) {
+    console.log(e.target.value);
+});
