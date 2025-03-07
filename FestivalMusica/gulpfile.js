cb5cb28446645424d767b46819627f82dcb625e3
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-03-06 22:04:16 -0500

    Video 167. Agregando Google Fonts

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest } from 'gulp';
+import { src, dest, watch } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
@@ -9,4 +9,8 @@ export function css( done ) {
     .pipe(sass())
     .pipe(dest('dist/css'))
   done()
+}
+
+export function dev(){
+  watch('src/scss/app.scss', css)
 }
