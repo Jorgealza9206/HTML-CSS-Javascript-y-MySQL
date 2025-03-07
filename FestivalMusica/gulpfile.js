4f140ea084c2bee1f514160c69f6b1096c671d3b
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-03-06 21:38:32 -0500

    Video 165. Compilando SASS con Gulp

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,5 +1,12 @@
-export function hola( done) {
-  console.log('Hola desde Gulpfile.js');
+import { src, dest } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
-  done();
+const sass = gulpSass( dartSass);
+
+export function css( done ) {
+  src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('dist/css'))
+  done()
 }
