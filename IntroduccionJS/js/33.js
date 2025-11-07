733a0764cd6ac0dc10e4ea18e20d0d4d264207f2
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-11-06 20:22:58 -0500

    Intentamos quitar el certificado de Live Server y buscamos por qué el código de 33.js no ejecutaba el fetch API, me demoré buscando la ruta, video 147, sección 10. Minuto 8

--- IntroduccionJS/js/33.js
+++ IntroduccionJS/js/33.js
@@ -1,18 +1,30 @@
-async function obtenerEmpleados() {
+function obtenerEmpleados() {
 
-    const archivo = 'empleados.json';
+    const archivo = './js/empleados.json';
 
-    // fetch(archivo)
-    //     .then( resultado => resultado.json())
-    //     .then( datos => {
-    //         // console.log(datos.empleados);
-
-    //         const { empleados } = datos;
-    //        console.log(empleados);
-    //     });
-
-    const resultado = await fetch(archivo);
-    const datos = await resultado.json();
-    console.log(datos);
+    fetch(archivo)
+        .then( resultado => console.log(resultado))
 }
+
+
+// async function obtenerEmpleados() {
+
+//     const archivo = 'empleados.json';
+
+//     fetch(archivo)
+//         .then( resultado => console.log(resultado))
+
+//     // fetch(archivo)
+//     //     .then( resultado => resultado.json())
+//     //     .then( datos => {
+//     //         // console.log(datos.empleados);
+
+//     //         const { empleados } = datos;
+//     //        console.log(empleados);
+//     //     });
+
+//     const resultado = await fetch(archivo);
+//     const datos = await resultado.json();
+//     console.log(datos);
+// }
 obtenerEmpleados();
