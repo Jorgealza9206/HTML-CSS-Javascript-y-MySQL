f8ee83e2d9ff87a34dd4d85ba1a4f42ee008b08b
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-11-22 13:12:51 -0500

    Video 143. Sección 11 Finalizado

--- IntroduccionJS/js/33.js
+++ IntroduccionJS/js/33.js
@@ -1,31 +1,43 @@
-function obtenerEmpleados() {
+// function obtenerEmpleados() {
+
+//     const archivo = './js/empleados.json';
+
+    // fetch(archivo)
+    //     .then( resultado =>  resultado.json() )
+    //     .then( datos => { 
+    //         //console.log(datos.empleados)
+
+    //         const { empleados } = datos;
+    //        //console.log(empleados);
+
+    //        empleados.forEach( empleado => {
+    //             console.log(empleado.nombre);
+
+    //             //document.querySelector('.contenido').textContent += empleado.nombre;
+    //        });
+    //     });
+        
+// }
+
+
+async function obtenerEmpleados() {
 
     const archivo = './js/empleados.json';
 
-    fetch(archivo)
-        .then( resultado =>  resultado.json() )
-        .then( datos => console.log(datos) );
+    // fetch(archivo)  //Función para enviar el response a 'resultado'
+    //     .then( resultado => console.log(resultado))
+
+    // fetch(archivo)
+    //     .then( resultado => resultado.json())
+    //     .then( datos => {
+    //         // console.log(datos.empleados);
+
+    //         const { empleados } = datos;
+    //        console.log(empleados);
+    //     });
+
+    const resultado = await fetch(archivo); //El resultado de fetch se guarda en 'resultado'
+    const datos = await resultado.json();
+    console.log(datos);
 }
-
-
-// async function obtenerEmpleados() {
-
-//     const archivo = 'empleados.json';
-
-//     fetch(archivo)
-//         .then( resultado => console.log(resultado))
-
-//     // fetch(archivo)
-//     //     .then( resultado => resultado.json())
-//     //     .then( datos => {
-//     //         // console.log(datos.empleados);
-
-//     //         const { empleados } = datos;
-//     //        console.log(empleados);
-//     //     });
-
-//     const resultado = await fetch(archivo);
-//     const datos = await resultado.json();
-//     console.log(datos);
-// }
 obtenerEmpleados();
